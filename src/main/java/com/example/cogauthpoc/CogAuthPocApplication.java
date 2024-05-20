b1508de8dabb3b0c74e7271c20a00a0f93b679fe
Souyama <souyamadebnath@gmail.com>
2024-05-20 06:14:28 +0000

    Init

---
+++ src/main/java/com/example/cogauthpoc/CogAuthPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.cogauthpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogAuthPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogAuthPocApplication.class, args);
+	}
+
+}
